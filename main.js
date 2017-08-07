b69197b1521a704ee6ba021af088a746d5e2e4ba
Matt Hensley <matt.hensley777@gmail.com>
2017-08-07 18:12:11 -0500

    working on if else statement for colors

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 var userInput = document.getElementById("userInput");
 var button = document.getElementById("converter");
 var results = document.getElementById("convertedvalue").value;
-// var clearAF = document.getElementById("clearButton").value = "";
+// var clearAF = document.getElementById("clearButton").value;
 
-
-function toCelsius() {
+function toCelsius(temp) {
     var fahrenheitToC = parseInt((userInput.value - 32) * (5 / 9));
-    // if 
     document.getElementById("convertedvalue").innerHTML = fahrenheitToC;
+    var classColor = toCelsius;
 }
 
-function toFahrenheit() {
+function toFahrenheit(temp) {
     var celsiusToF = parseInt((userInput.value * (9 / 5) + 32));
-    
-    document.getElementById("convertedvalue").innerHTML = celsiusToF;
+	document.getElementById("convertedvalue").innerHTML = celsiusToF;
+	var classColor = toFahrenheit;
 }
-
 // This function should determine which conversion should
 // happen based on which radio button is selected.
-function determineConverter(clickEvent) {
+function determineConverter(temp) {
     if (document.getElementById("FtoC").checked) {
         toCelsius()
-    } else {
-        if (document.getElementById("CtoF").checked) {
-            toFahrenheit()
-        }
+    } else if (document.getElementById("CtoF").checked) {
+        toFahrenheit()
     }
 }
 
 
 
 document.getElementById("convertbutton").addEventListener("click", determineConverter);
+// document.getElementById("clearButton").addEventListener("clear", clearInput);
+
+// function clearInput() {
+//     convertedvalue.innerHTML = document.getElementById("userInput").value = "";
+//     }
